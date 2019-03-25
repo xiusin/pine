80948d3a9532b36aa1ec23f7b2d2d023c36bdf8e
xiusin <chenchengbin92@gmail.com>
2019-03-25 18:40:31 +0800

    引入 Binding swagger, 删除beego

---
+++ README.md
@@ -0,0 +1,6 @@
+1. hook 或 event 事件
+2. di容器如何实现
+3. session驱动
+4. cache 驱动
+5. 反射机制
+6. mvc 模式
---
+++ examples/swagger.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	httpSwagger "github.com/swaggo/http-swagger"
+	"router/core"
+	"router/examples/docs"
+)
+
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host petstore.swagger.io
+// @BasePath /v2
+func main() {
+	handler := core.NewRouter()
+
+	docs.SwaggerInfo.Title = "Swagger Example API"
+	docs.SwaggerInfo.Description = "This is a sample server Petstore server."
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = "petstore.swagger.io"
+	docs.SwaggerInfo.BasePath = "/v2"
+
+	handler.GET("/hello/:name", hello)
+
+	handler.GET("/swagger/*any", func(context *core.Context) {
+		httpSwagger.WrapHandler(context.Writer(), context.Request())
+	})
+
+	handler.Serve("0.0.0.0:9999")
+
+}
+
+// ShowAccount godoc
+// @Summary Show a account
+// @Description get string by ID
+// @ID get-string-by-int
+// @Accept  json
+// @Produce  json
+// @Param id path int true "Account ID"
+// @Router /accounts/{id} [get]
+func hello(c *core.Context) {
+	_, _ = c.Writer().Write([]byte("Hello " + c.GetParamDefault("name", "world")))
+}
