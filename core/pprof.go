c39fb3427f9546dec4376c8571c6678e9f650402
xiusin <chenchengbin92@gmail.com>
2019-05-11 18:32:34 +0800

    队列模块

--- core/pprof.go
+++ core/pprof.go
@@ -24,6 +24,9 @@ func pprofTrace(context *Context) {
 	pprof.Trace(context.Writer(), context.Request())
 }
 
+
+
+//https://blog.cyeam.com/golang/2016/08/18/apatternforoptimizinggo
 func EnableProfile(r *Router) {
 	r.GET("/debug/pprof/*action", pprofIndex)
 	r.GET("/debug/pprof/cmdline", pprofCmdline)
--- examples/test.go
+++ examples/test.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
-	"regexp"
-	"strings"
 )
 
 func main()  {
-	compiler := regexp.MustCompile(`([A-Z])`)
-	fmt.Println(compiler.ReplaceAllStringFunc("GetEditName", func(s string) string {
-		fmt.Println(s)
-		return strings.ToLower("_" + s)
-	}))
+	//compiler := regexp.MustCompile(`([A-Z])`)
+	//fmt.Println(compiler.ReplaceAllStringFunc("GetEditName", func(s string) string {
+	//	fmt.Println(s)
+	//	return strings.ToLower("_" + s)
+	//}))
+
+	fmt.Println(uint(-20))
+
 }
