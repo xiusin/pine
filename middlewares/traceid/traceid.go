bf23856aa4d85ccc36dd3ac2957a4c9548db3326
xiusin <826466266@qq.com>
2022-02-11 17:30:43 +0800

    日志

--- middlewares/traceid/traceid.go
+++ middlewares/traceid/traceid.go
@@ -1,13 +1,18 @@
 package traceid
 
 import (
-	uuid "github.com/satori/go.uuid"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
 	"github.com/xiusin/pine"
+	"time"
 )
 
 func TraceId() pine.Handler {
 	return func(ctx *pine.Context) {
-		traceId := uuid.NewV4().String()
+		hash := md5.New()
+		hash.Write([]byte(fmt.Sprintf("%d", time.Now().UnixNano())))
+		traceId := hex.EncodeToString(hash.Sum(nil))
 		ctx.Response.Header.Set("Req-Trace-ID", traceId)
 		ctx.LoggerEntity().Id(traceId)
 		ctx.Next()
